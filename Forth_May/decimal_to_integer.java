d36247f712240f62f0c3539d3e48c280cfd02006
balkrishnapithadiya <83627849+balkrishnapithadiya@users.noreply.github.com>
2021-05-04 18:05:54 +0530

    Add files via upload

---
+++ Forth_May/decimal_to_integer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class decimal_to_integer {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("enter the decimal number");
+	        float a =sc.nextFloat();
+	        int b = (int) a;
+	        System.out.println("Decimal" + a + " is now Integer" + b);
+
+	}
+
+}
